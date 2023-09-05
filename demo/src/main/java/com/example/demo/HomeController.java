0ddecf48e38a7363a66d042de38ef4570e68a9de
back <kjkjk0@naver.com>
2023-09-05 10:13:25 +0900

    chat add

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import java.util.Date;
 import java.util.Iterator;
 
+import org.apache.logging.log4j.message.SimpleMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +17,19 @@ public class HomeController {
 	@Autowired
 	CustomerRepository repo;
 	
-	@GetMapping("/")
-	public String greet() {
-		return "hello";
-	}
-	
+	@Autowired
+	//private SimpleMessageTemplate template;
+
 	@GetMapping("/cust")
 	public Iterator<Customer> cust(){
+		String text = "[" + new Date() + "]" + "cust selset";
+		//this.template.convertAndSend("/topic/cust", new Greeting(text));
 		return repo.findAll().iterator();
 	}
+	
+	
+	
+	
+	
+	
 }
--- demo/src/main/java/com/example/demo/customer/CustomerRepository.java
+++ demo/src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long>{
-	
-	
+
 	List<Customer> findByName(String name);
 }
